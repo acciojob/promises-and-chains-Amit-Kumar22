f120ad8931fe1b389e5248af5ab67e6fd4de378f
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-08 23:10:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ let submit=document.getElementById("btn");
 btn.addEventListener("click",()=>{
 	let ageval=ageinp.value;
 	let nameval=nameinp.value;
-	if(ageval>=18){
+	let promise= new Promise((resolve,reject)=>{
+		if(ageval>=18){
 		setTimeout(()=>{
-		alert("Welcome,"+nameval+" You can vote.")
+		resolve(alert("Welcome, "+nameval+". You can vote."))
 	},4000)
 	}
 	else{
-		alert("Oh sorry "+nameval+" You aren't old enough.")
+		alert("Oh sorry. "+nameval+" You aren't old enough.")
 	}
+	})
 })
 
