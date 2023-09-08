86cab65dcc7730753255e52a1a01077d8ca5f651
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-08 22:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let ageinp=document.getElementById("age");
+let nameinp=document.getElementById("name");
+
+let submit=document.getElementById("btn");
+
+btn.addEventListener("click",()=>{
+	let ageval=ageinp.value;
+	let nameval=nameinp.value;
+	if(ageval>=18){
+		setTimeout(()=>{
+		alert("Welcome,"+nameval+" You can vote.")
+	},4000)
+	}
+	else{
+		alert("Oh sorry "+nameval+" You aren't old enough.")
+	}
+})
+
