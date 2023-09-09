0ff71cd8f249a76aef398d73149d0752d8d0bbb7
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-09 07:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ btn.addEventListener("click",()=>{
 		resolve(alert("Welcome, "+nameval+". You can vote."))
 	},4000)
 	}
-	else if(ageval<18){
-		alert("Oh sorry "+nameval+". You aren't old enough.")
+	else if(ageval<18 && ageval>0){
+		rejection(alert("Oh sorry "+nameval+". You aren't old enough."))
 	}
+		else{
+			alert("Please enter valid details");
+		}
 	})
 })
 
